109e151fd57575bebdb235ff609bf07fda2e7d42
Chiang <wei.hoe.chiang@accenture.com>
2022-05-24 15:47:35 +0100

    fix: update api headers

--- src/app/services/gov-food-ratings.service.ts
+++ src/app/services/gov-food-ratings.service.ts
@@ -7,24 +7,27 @@ import { IRegionsResponse } from '../interfaces/regions.interface';
 
 @Injectable({ providedIn: 'root' })
 export class GoodFoodRatingsService {
-  headers = new HttpHeaders();
-  constructor(private http: HttpClient) {
-    this.headers.set('x-api-version', '2');
-    this.headers.set('content-type', 'application/json');
-    this.headers.set('accept', 'application/json');
-  }
+  constructor(private http: HttpClient) {}
 
   public getRegions(): Observable<IRegionsResponse> {
     return this.http.get<IRegionsResponse>(
       'https://api.ratings.food.gov.uk/regions',
-      { headers: this.headers }
+      {
+        headers: {
+          'x-api-version': '2',
+        },
+      }
     );
   }
 
   public getAuthorities(): Observable<IAuthoritiesResponse> {
     return this.http.get<IAuthoritiesResponse>(
       'https://api.ratings.food.gov.uk/regions',
-      { headers: this.headers }
+      {
+        headers: {
+          'x-api-version': '2',
+        },
+      }
     );
   }
 
@@ -33,7 +36,11 @@ export class GoodFoodRatingsService {
   ): Observable<IEstablishmentsResponse> {
     return this.http.get<IEstablishmentsResponse>(
       `https://api.ratings.food.gov.uk/regions?localAuthorityId=${localAuthorityId}&businessTypeId=1`,
-      { headers: this.headers }
+      {
+        headers: {
+          'x-api-version': '2',
+        },
+      }
     );
   }
 }
